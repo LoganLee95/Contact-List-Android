f54d924d22faf65535316b2f8cbf31b0c8fa0f44
Harry G. Dulaney <20808723+Nothingrhymeswithorange@users.noreply.github.com>
2019-09-11 11:17:20 -0400

    Add files via upload

--- MyContactList/app/src/main/java/com/example/mycontactlist/ContactSettingsActivity.java
+++ MyContactList/app/src/main/java/com/example/mycontactlist/ContactSettingsActivity.java
@@ -2,7 +2,10 @@ package com.example.mycontactlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ContactSettingsActivity extends AppCompatActivity {
 
@@ -10,5 +13,52 @@ public class ContactSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_settings);
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+    }
+    private void initListButton() {
+
+        ImageButton imageButtonList = (ImageButton) findViewById(R.id.imageButtonList);
+        imageButtonList.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void initMapButton() {
+
+        ImageButton mapButton = (ImageButton) findViewById(R.id.imageButtonMap);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void initSettingsButton() {
+
+        ImageButton imageButtonList = (ImageButton) findViewById(R.id.imageButtonSettings);
+        imageButtonList.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactSettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
     }
 }
